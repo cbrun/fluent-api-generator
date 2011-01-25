ad7a9ac1f7c77e89d77a5bdf17e59bbecbf59459
Cedric Brun <cedric.brun@obeo.fr>
2011-01-25 17:45:24 +0100

    Updated a few source code header adding the EPL

--- fr.obeo.dsl.fluent.tests/src/TestAPI.java
+++ fr.obeo.dsl.fluent.tests/src/TestAPI.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2008, 2010 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
 import org.eclipse.emf.common.util.URI;
 
 import fr.obeo.dsl.viewpoint.tests.support.DesignerBot;
